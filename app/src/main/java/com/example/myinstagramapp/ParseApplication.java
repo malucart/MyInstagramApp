264b906ddc3979e4abdeba3160304b3637b9743c
malucart <malucartaxo_@hotmail.com>
2021-07-07 21:31:27 -0700

    Authentication working well

---
+++ app/src/main/java/com/example/myinstagramapp/ParseApplication.java
@@ -0,0 +1,20 @@
+package com.example.myinstagramapp;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+public class ParseApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId(getString(R.string.back4app_app_id))
+                .clientKey(getString(R.string.back4app_client_key))
+                .server("https://parseapi.back4app.com")
+                .build()
+        );
+    }
+}
